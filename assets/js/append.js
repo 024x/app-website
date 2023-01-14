8095207d7182e57b27cb321d772a3b3b9c75c213
024x <satyendrabongi@gmail.com>
2023-01-14 12:16:55 +0530

    main

--- assets/js/append.js
+++
@@ -1,3 +0,0 @@
-tovp = document.getElementById("app-users");
-hide.innerHTML = "<h3>Verification success!<br> please Close This tab or window</h3>";
-tovp.innerHTML = "<h3>Verification success!<br> please Close This tab or window</h3>";
